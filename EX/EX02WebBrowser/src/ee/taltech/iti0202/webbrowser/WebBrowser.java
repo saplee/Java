4d1589516427e39157edb603be49535b2c9752d5
Sander Pleesi <saplee@ttu.ee>
2022-02-02 22:30:35 +0200

    java

--- EX/EX02WebBrowser/src/ee/taltech/iti0202/webbrowser/WebBrowser.java
+++ EX/EX02WebBrowser/src/ee/taltech/iti0202/webbrowser/WebBrowser.java
@@ -132,9 +132,6 @@ public class WebBrowser {
             if (dict.get(key) >= number) {
                 result.add(0, key);
                 number = dict.get(key);
-            }else if (dict.get(key) == number){
-                result.add(1, key);
-                number = dict.get(key);
             }
         }
         for (String pages : result) {
