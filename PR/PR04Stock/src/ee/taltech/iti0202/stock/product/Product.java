bfaca8d2e492588542e6de5941edae436a8a2170
Sander Pleesi <saplee@ttu.ee>
2022-02-13 17:58:09 +0200

    java

---
+++ PR/PR04Stock/src/ee/taltech/iti0202/stock/product/Product.java
@@ -0,0 +1,63 @@
+package ee.taltech.iti0202.stock.product;
+import ee.taltech.iti0202.stock.exceptions.StockException;
+
+public class Product {
+
+    private String name;
+    private int price;
+    private int id;
+    private static int number = -1;
+
+    /**
+     * Create a new product with the given name and price.
+     * <p>
+     * If the price is negative, throw a StockException with a NEGATIVE_PRICE reason.
+     *
+     * @param name Name of the product.
+     * @param price Price of the product.
+     * @throws StockException NEGATIVE_PRICE
+     */
+    public Product(String name, int price) throws StockException {
+        this.name = name;
+        this.price = price;
+        this.id = getNextId();
+    }
+
+    /**
+     * Returns the next id.
+     *
+     * This value has to increment with every call.
+     *
+     * @return The next id.
+     */
+    public static int getNextId() {
+        return ++number;
+    }
+
+    /**
+     * Returns id of the product.
+     * 
+     * @return id of the product.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Returns the name of the product.
+     * 
+     * @return the name of the product.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the price of the product.
+     * 
+     * @return the price of the product.
+     */
+    public int getPrice() {
+        return price;
+    }
+}
---
+++ PR/PR04Stock/src/ee/taltech/iti0202/stock/stock/Stock.java
@@ -0,0 +1,121 @@
+package ee.taltech.iti0202.stock.stock;
+import ee.taltech.iti0202.stock.exceptions.StockException;
+import ee.taltech.iti0202.stock.product.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * The stock class.
+ * <p>
+ * Each stock has a name, list of products that are currently stored in it
+ * and the maximum amount of products that stock can store.
+ * <p>
+ * If adding a product to the stock is not possible, due to exceeding the maximum limit of products
+ * OR stock already contains a product, a StockException must be thrown,
+ * otherwise product must be added to the stock.
+ * <p>
+ * When getting (not removing) a product from our stock,
+ * it is important to find the product with the lowest price first.
+ */
+
+public class Stock {
+
+    private String name;
+    private int maxCapacity;
+
+    /**
+     * Create a new stock with the given name and the max capacity for the products.
+     * 
+     * @param name the name of the stock.
+     * @param maxCapacity max amount of products allowed in the stock.
+     */
+    public Stock(String name, int maxCapacity) {
+        this.name = name;
+        this.maxCapacity = maxCapacity;
+    }
+
+    /**
+     * Add a product to the stock, if stock does not contain the product and is not full yet.
+     * <p>
+     * Check in following order:
+     * If stock already contains a product, throw an StockException with a STOCK_ALREADY_CONTAINS_PRODUCT reason.
+     * If stock is full, throw a StockException with a STOCK_IS_FULL reason.
+     *
+     * @param product to be added
+     * @throws StockException STOCK_ALREADY_CONTAINS_PRODUCT, STOCK_IS_FULL
+     */
+
+    public void addProduct(Product product) throws StockException {
+    }
+
+    /**
+     * Get a product from a stock by name with the lowest price.
+     *
+     * If there are several products with the same name and price,
+     * returns the product with the lowest id.
+     *
+     * @param name the product's name
+     * @return Optional
+     */
+    public Optional<Product> getProduct(String name) {
+        return Optional.empty();
+    }
+
+    /**
+     * Remove and return a product from a stock,
+     * if stock has a given product.
+     *
+     * Use getProduct() method to get the product.
+     * 
+     * If there is nothing to remove, return Optional.empty()
+     *
+     * @param name Name of the product to be removed
+     * @return Optional
+     */
+
+    public Optional<Product> removeProduct(String name) {
+        return Optional.empty();
+    }
+
+    /**
+     * Get a list of current products in the stock.
+     *
+     * @return List
+     */
+    public List<Product> getProducts() {
+        return null;
+    }
+
+    /**
+     * Get a list of current products in the stock filtered by name.
+     *
+     * Order the products by price ascending. In case of the same price, by id ascending.
+     *
+     * @param name Name to be filtered.
+     * @return List
+     */
+    public List<Product> getProducts(String name) {
+        return null;
+    }
+
+    /**
+     * Get total price of all the products.
+     *
+     * @return Total price.
+     */
+    public int getTotalPrice() {
+        return -1;
+    }
+
+    /**
+     * Check if stock is full.
+     *
+     * @return boolean
+     */
+    public boolean isFull() {
+        return false;
+    }
+
+}
+
