838a7d9aa8ea926e08c507e3557c46d572e75426
Sander Pleesi <saplee@ttu.ee>
2022-02-14 15:38:19 +0200

    java

--- PR/PR04Stock/src/ee/taltech/iti0202/stock/stock/Stock.java
+++ PR/PR04Stock/src/ee/taltech/iti0202/stock/stock/Stock.java
@@ -66,7 +66,14 @@ public class Stock {
      * @return Optional
      */
     public Optional<Product> getProduct(String name) {
-        return Optional.empty();
+        Product result = null;
+        int number = 1000000000;
+        for (Product product : stockList) {
+            if (product.getName().equals(name) && product.getPrice() <= number) {
+                result = product;
+            }
+        }
+        return Optional.ofNullable(result);
     }
 
     /**
@@ -82,7 +89,15 @@ public class Stock {
      */
 
     public Optional<Product> removeProduct(String name) {
-        return Optional.empty();
+        Product result = null;
+        int number = 1000000000;
+        for (Product product : stockList) {
+            if (product.getName().equals(name) && product.getPrice() <= number) {
+                result = product;
+            }
+        }
+        stockList.remove(result);
+        return Optional.ofNullable(result);
     }
 
     /**
@@ -128,5 +143,47 @@ public class Stock {
         }
     }
 
+    public static void main(String[] args) throws StockException {
+        Stock fruitStock = new Stock("fruit-stock-1", 4);
+
+        Product cheapApple = new Product("apple", 3);
+        Product expensiveApple = new Product("apple", 9);
+        Product orange = new Product("orange", 5);
+        Product mango = new Product("mango", 6);
+        Product pear = new Product("pear", 4);
+        System.out.println(pear.getId()); // 5
+
+        fruitStock.addProduct(expensiveApple);
+        fruitStock.addProduct(cheapApple);
+        System.out.println(fruitStock.getProducts()); // expensiveApple, cheapApple
+
+        Optional<Product> apple = fruitStock.getProduct("apple"); // Optional.of(cheapApple)
+        apple.ifPresent(System.out::println); // cheapApple
+
+        fruitStock.addProduct(orange);
+        fruitStock.addProduct(mango);
+        System.out.println(fruitStock.getProducts().size()); // 4
+        System.out.println(fruitStock.getProducts("apple")); // cheapApple, expensiveApple
+
+        try {
+            fruitStock.addProduct(pear);
+        } catch (StockException e) {
+            System.out.println(e.getReason()); // STOCK_IS_FULL
+        }
+
+        try {
+            fruitStock.addProduct(mango);
+        } catch (StockException e) {
+            System.out.println(e.getReason()); // STOCK_ALREADY_CONTAINS_PRODUCT
+        }
+
+        Optional<Product> removedMango = fruitStock.removeProduct("mango"); // Optional.of(mango)
+        removedMango.ifPresent(System.out::println);
+
+        System.out.println(fruitStock.removeProduct("apple")); // Optional[cheapApple]
+        System.out.println(fruitStock.removeProduct("apple").get()); // Optional[expensiveApple]
+        System.out.println(fruitStock.removeProduct("dumpling")); // Optional.empty
+
+    }
 }
 
