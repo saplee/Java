917ec450c1ce81973b7caa179ead126da1c5b4eb
Sander Pleesi <saplee@ttu.ee>
2022-02-17 19:55:36 +0200

    java

---
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/BookTest.java
@@ -0,0 +1,95 @@
+package ee.taltech.iti0202.bookshelf;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+    final int number = 500;
+    final int price = 500;
+    final int price2 = 400;
+    final int price3 = 800;
+    final int year = 1926;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setOwner() {
+        Person person = new Person("Kalev", number);
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price);
+        book.setOwner(person);
+        assertEquals(number, book.getOwner().getMoney());
+    }
+
+    @Test
+    void getTitle() {
+        Person person = new Person("Kalev", number);
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price);
+        book.setOwner(person);
+        assertEquals("Truth and Justice", book.getTitle());
+    }
+
+    @Test
+    void getAuthor() {
+        Person person = new Person("Kalev", number);
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price);
+        book.setOwner(person);
+        assertEquals("Tammsaare", book.getAuthor());
+    }
+
+    @Test
+    void buyBook() {
+        Person person = new Person("Kalev", number);
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price3);
+        assertFalse(book.buy(person));
+    }
+
+    @Test
+    void buyBook2() {
+        Person person = new Person("Kalev", number);
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price2);
+        assertTrue(book.buy(person));
+    }
+
+    @Test
+    void getPrice() {
+        Person person = new Person("Kalev", number);
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price);
+        book.setOwner(person);
+        assertEquals(price, book.getPrice());
+    }
+
+    @Test
+    void getOwner2() {
+        Person person = new Person("Kalev", number);
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price);
+        book.setOwner(person);
+        assertEquals("Kalev", book.getOwner().getName());
+    }
+
+    @Test
+    void getYearOfPublishing() {
+        Person person = new Person("Kalev", number);
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price);
+        book.setOwner(person);
+        assertEquals(year, book.getYearOfPublishing());
+    }
+
+    @Test
+    void buyBook3() {
+        Person person = null;
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price3);
+        assertFalse(book.buy(person));
+    }
+
+    @Test
+    void buyBook4() {
+        Person person = new Person("Kalev", number);
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price3);
+        book.setOwner(person);
+        assertFalse(book.buy(person));
+    }
+}
---
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/PersonTest.java
@@ -0,0 +1,62 @@
+package ee.taltech.iti0202.bookshelf;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ */
+class PersonTest {
+    final int number = 45;
+    final int number2 = 400;
+    final int number3 = 199;
+    final int price = 200;
+    final int year = 1926;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getMoney() {
+        Person person = new Person("Kalle", number);
+        assertEquals(number, person.getMoney());
+    }
+
+    @Test
+    void getName() {
+        Person person = new Person("Kalle", number);
+        assertEquals("Kalle", person.getName());
+    }
+
+    @Test
+    void buyBook() {
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price);
+        Person person = new Person("Kalle", number2);
+        assertTrue(person.buyBook(book));
+    }
+
+    @Test
+    void buyBook2() {
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price);
+        Person person = new Person("Kalle", number3);
+        assertFalse(person.buyBook(book));
+    }
+
+    @Test
+    void sellBook() {
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price);
+        Person person = new Person("Kalle", number3);
+        assertFalse(person.sellBook(book));
+    }
+
+    @Test
+    void sellBook2() {
+        Book book = new Book("Truth and Justice", "Tammsaare", year, price);
+        Person person = new Person("Kalle", number3);
+        book.setOwner(person);
+        assertTrue(person.sellBook(book));
+    }
+}
