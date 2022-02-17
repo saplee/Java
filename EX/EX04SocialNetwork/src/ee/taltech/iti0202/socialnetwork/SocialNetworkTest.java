7847a05e001c99162b82bd7da8306fe12b52fefd
Sander Pleesi <saplee@ttu.ee>
2022-02-17 20:05:09 +0200

    java

--- EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetworkTest.java
+++ EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetworkTest.java
@@ -1,4 +1,5 @@
 package ee.taltech.iti0202.socialnetwork;
+
 import ee.taltech.iti0202.socialnetwork.group.Group;
 import ee.taltech.iti0202.socialnetwork.user.User;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,14 +12,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SocialNetworkTest {
     final int age = 19;
+
     @BeforeEach
     void setUp() {
     }
+
     @Test
     void getGroups() {
         SocialNetwork socialNetwork = new SocialNetwork();
         User user = new User("Joosep", age);
-        Group group =new Group("Proge", user);
+        Group group = new Group("Proge", user);
         socialNetwork.registerGroup(group);
         Set<Group> groups = new HashSet<>(Set.of(group));
         assertEquals(groups, socialNetwork.getGroups());
