7269aa6cdcfff1462a6b64feaaae1ddacc3fe4ae
Sander Pleesi <saplee@ttu.ee>
2022-02-21 20:40:01 +0200

    java

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/storage/ResourceStorage.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/storage/ResourceStorage.java
@@ -1,14 +1,27 @@
 package ee.taltech.iti0202.mysticorbs.storage;
 
+import java.util.LinkedHashMap;
+import java.util.Set;
+
 public class ResourceStorage {
+    private LinkedHashMap<String, Integer> resourceMap = new LinkedHashMap<>();
     public boolean isEmpty() {
-        return true;
+        Set<String> keys = resourceMap.keySet();
+        for (String key:keys){
+            if (resourceMap.get(key) > 0){
+                return true;
+            }
+        }return false;
     }
 
     public void addResource(String resource, int amount) {
+        resourceMap.put(resource, resourceMap.getOrDefault(resource, amount) + amount);
     }
 
     public int getResourceAmount(String resource) {
+        if (resourceMap.containsKey(resource)){
+            return resourceMap.get(resource);
+        }
         return 0;
     }
 
