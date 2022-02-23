35653f3e7bab029da17f9002a070206b79f207d2
Sander Pleesi <saplee@ttu.ee>
2022-02-23 15:14:17 +0200

    java

--- TK/TK1/src/ee/taltech/iti0202/tk1/Exam.java
+++ TK/TK1/src/ee/taltech/iti0202/tk1/Exam.java
@@ -73,7 +73,7 @@ public class Exam {
     /**
      * Modify and return the given map as follows: if the keys "a" and "b" are both in the map
      * and have equal values, remove them both.
-     * <p>
+     * <p>a
      * mapAB({"a": "aaa", "b": "aaa", "c": "cake"}) → {"c": "cake"}
      * mapAB({"a": "aaa", "b": "bbb"}) → {"a": "aaa", "b": "bbb"}
      * mapAB({"a": "aaa", "b": "bbb", "c": "aaa"}) → {"a": "aaa", "b": "bbb", "c": "aaa"}
