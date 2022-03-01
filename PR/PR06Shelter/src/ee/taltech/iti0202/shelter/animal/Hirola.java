5aa42a41d659951d732978392349a8105c0196b3
Sander Pleesi <saplee@ttu.ee>
2022-03-01 15:47:44 +0200

    java

---
+++ PR/PR06Shelter/src/ee/taltech/iti0202/shelter/animal/Hirola.java
@@ -0,0 +1,10 @@
+package ee.taltech.iti0202.shelter.animal;
+
+/**
+ * https://en.wikipedia.org/wiki/Hirola
+ */
+public class Hirola extends Animal {
+    public Hirola(String color) {
+        super(color);
+    }
+}
---
+++ PR/PR06Shelter/src/ee/taltech/iti0202/shelter/animal/Wombat.java
@@ -0,0 +1,10 @@
+package ee.taltech.iti0202.shelter.animal;
+
+/**
+ * https://en.wikipedia.org/wiki/Wombat
+ */
+public class Wombat extends Animal {
+    public Wombat(String color) {
+        super(color);
+    }
+}
