47b92630be9902e27b8cbc1ce8b2b21067db69cf
Sander Pleesi <saplee@ttu.ee>
2022-03-02 21:29:07 +0200

    java

--- EX/EX06Files/src/ee/taltech/iti0202/files/input/FileReaderException.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/input/FileReaderException.java
@@ -1,4 +1,11 @@
 package ee.taltech.iti0202.files.input;
 
 public class FileReaderException {
+    public Throwable getCause() {
+        return null;
+    }
+
+    public String getMessage() {
+        return "No such file";
+    }
 }
