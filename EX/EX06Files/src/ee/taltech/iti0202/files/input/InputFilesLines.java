b5d296d14ce58a73ecf759dd271436394e86f32b
Sander Pleesi <saplee@ttu.ee>
2022-03-03 16:08:26 +0200

    java

--- EX/EX06Files/src/ee/taltech/iti0202/files/input/InputFilesLines.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/input/InputFilesLines.java
@@ -1,11 +1,29 @@
 package ee.taltech.iti0202.files.input;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class InputFilesLines implements InputFilesReader {
 
     @Override
     public List<String> readTextFromFile(String filename) {
-        return null;
+        Path path = Paths.get(filename);
+        List<String> result = new ArrayList<>();
+        try {
+            while (true) {
+                Stream<String> lines = Files.lines(path);
+                lines.forEach(s -> result.add(s));
+                if (lines == null) break;
+            }
+        } catch (IOException e) {
+            throw new FileReaderException("No such file", e);
+        }
+        return result;
     }
 }
--- EX/EX06Files/src/ee/taltech/iti0202/files/input/InputFilesScanner.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/input/InputFilesScanner.java
@@ -1,11 +1,29 @@
 package ee.taltech.iti0202.files.input;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class InputFilesScanner implements InputFilesReader {
 
     @Override
     public List<String> readTextFromFile(String filename) {
-        return null;
+        Path path = Paths.get(filename);
+        List<String> result = new ArrayList<>();
+        try (Scanner scanner = new Scanner(path)) {
+            while (scanner.hasNextLine()) {
+                if (scanner.hasNextLine()) {
+                    result.add(scanner.nextLine());
+                } else {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            throw new FileReaderException("No such file", e);
+        }
+        return result;
     }
 }
