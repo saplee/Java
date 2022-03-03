3362bc27863b7610072eddb38fd064286e359c37
Sander Pleesi <saplee@ttu.ee>
2022-03-03 17:57:39 +0200

    java

--- EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
@@ -1,12 +1,19 @@
 package ee.taltech.iti0202.files.morse;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MorseTranslator {
+    private HashMap<String, String> map = new HashMap<>();
 
     public Map<String, String> addMorseCodes(List<String> lines) {
-        return null;
+        for (String line : lines) {
+            List<String> list = new ArrayList<>(List.of(line.split(" ")));
+            map.put(list.get(0), list.get(1));
+        }
+        return map;
     }
 
     public List<String> translateLinesToMorse(List<String> lines) {
--- EX/EX06Files/src/ee/taltech/iti0202/files/output/OutputFilesWriter.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/output/OutputFilesWriter.java
@@ -1,10 +1,27 @@
 package ee.taltech.iti0202.files.output;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class OutputFilesWriter {
 
     public boolean writeLinesToFile(List<String> lines, String filename) {
-        return false;
+        if (filename == null) {
+            return false;
+        }
+        Path path = Paths.get(filename);
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            for (String line : lines) {
+                writer.write(line);
+            }
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+}
 }
