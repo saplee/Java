d74cfbbdd86841ae4ba966550f40ca6f7e55f8c8
Sander Pleesi <saplee@ttu.ee>
2022-03-04 16:00:36 +0200

    java

--- EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
@@ -1,9 +1,17 @@
 package ee.taltech.iti0202.files.morse;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static java.nio.file.Files.newBufferedReader;
 
 public class MorseTranslator {
     private HashMap<String, String> map = new HashMap<>();
@@ -24,6 +32,25 @@ public class MorseTranslator {
         }
         return map;
     }
+
+    public List<String> readTextFromFile(String filename) {
+        Path path = Paths.get(filename);
+        List<String> result = new ArrayList<>();
+        try (BufferedReader reader = newBufferedReader(path)) {
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) {
+                    break;
+                } else {
+                    result.add(line);
+                }
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return result;
+    }
+
 
     /**
      * @param lines
@@ -66,14 +93,23 @@ public class MorseTranslator {
 
     private String translateLineFromMorse(String line) {
         String result = "";
-        String word = line.toLowerCase().trim();
-        for (int i = 0; i < line.length(); i++) {
-            if (word.substring(i, i + 1).equals("\t")) {
+        List<String> list = new ArrayList<>(List.of(line.trim().split(" ")));
+        for (String morse : list) {
+            if (morse.contains("\t")) {
+                result += map2.get(morse.substring(0, morse.indexOf("\t")));
                 result += " ";
-            } else if (map2.containsKey(word.substring(i, i + 1))) {
-                result += map2.get(word.substring(i, i + 1));
+                result += map2.get(morse.substring(morse.indexOf("\t") + 1));
+            } else if (map2.containsKey(morse)) {
+                result += map2.get(morse);
             }
         }
         return result.trim();
+    }
+
+    public static void main(String[] args) {
+        List<String> lit = new ArrayList<>(List.of(".-.. --- .-. . --\t.. .--. ... ..- --\t-.. --- .-.. --- .-.\t... .. -\t.- -- . - --..--\t-.-. --- -. ... . -.-. - . - ..- .-.\t.- -.. .. .--. .. ... -.-. .. -. --.\t. .-.. .. - --..--"));
+        MorseTranslator morseTranslator = new MorseTranslator();
+        morseTranslator.addMorseCodes(morseTranslator.readTextFromFile("/Users/sanderpleesi1/Downloads/morse.txt"));
+        System.out.println(morseTranslator.translateLinesFromMorse(lit));
     }
 }
