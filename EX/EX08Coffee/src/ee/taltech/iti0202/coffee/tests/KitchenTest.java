404ee9a8f3555a03fb2ee77a711f826ba89bcdb5
Sander Pleesi <saplee@ttu.ee>
2022-03-21 18:08:45 +0200

    java

--- EX/EX08Coffee/src/ee/taltech/iti0202/coffee/tests/KitchenTest.java
+++ EX/EX08Coffee/src/ee/taltech/iti0202/coffee/tests/KitchenTest.java
@@ -1,7 +1,34 @@
 package ee.taltech.iti0202.coffee.tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import ee.taltech.iti0202.coffee.drinks.Drink;
+import ee.taltech.iti0202.coffee.exceptions.CapsuleAlreadyInside;
+import ee.taltech.iti0202.coffee.exceptions.EmptyWaterTankException;
+import ee.taltech.iti0202.coffee.exceptions.GarbageContainerFull;
+import ee.taltech.iti0202.coffee.exceptions.MachineException;
+import ee.taltech.iti0202.coffee.kitchen.Kitchen;
+import ee.taltech.iti0202.coffee.machine.CoffeeMachine;
+import ee.taltech.iti0202.coffee.water.WaterTank;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 class KitchenTest {
-
+    @Test
+    public void testMakeCoffee() throws MachineException, EmptyWaterTankException, GarbageContainerFull,
+            CapsuleAlreadyInside {
+        Kitchen kitchen = new Kitchen();
+        final int number = 10;
+        WaterTank waterTank = new WaterTank(100);
+        // Making coffee machine
+        CoffeeMachine coffeeMachine = new CoffeeMachine(waterTank, number, 1000);
+        Map<String, Integer> map = new HashMap<>();
+        // Making drink recipe, Coffee machine can only make drinks with coffee beans.
+        map.put("coffee beans", 1);
+        // Making drink with drink Type and recipe
+        kitchen.addCoffeeMachine(coffeeMachine);
+        Drink drink = new Drink(Drink.DrinkType.COFFEE, map);
+        Assertions.assertEquals(Drink.DrinkType.COFFEE,kitchen.makeDrink(drink, coffeeMachine));
+    }
 }
