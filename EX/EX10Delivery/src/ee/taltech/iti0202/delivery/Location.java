c65739ba36b5ae4882e398ea85c0b166e85159bf
Sander Pleesi <saplee@ttu.ee>
2022-04-02 00:45:22 +0300

    java

--- EX/EX10Delivery/src/ee/taltech/iti0202/delivery/Location.java
+++ EX/EX10Delivery/src/ee/taltech/iti0202/delivery/Location.java
@@ -1,18 +1,17 @@
 package ee.taltech.iti0202.delivery;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class Location {
-
-    public Location() {
-    }
-
+    private List<Packet> packets = new ArrayList<>();
     public Integer getDistanceTo(String name) {
         return Integer.MAX_VALUE;
     }
 
     public void addPacket(Packet packet) {
-
+        packets.add(packet);
     }
 
     public Optional<Packet> getPacket(String name) {
