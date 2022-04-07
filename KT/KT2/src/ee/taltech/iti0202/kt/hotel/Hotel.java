7a9d99bcb4c1c2f0061e27317fda19e97f8d743c
Sander Pleesi <saplee@ttu.ee>
2022-04-07 19:31:03 +0300

    java

--- KT/KT2/src/ee/taltech/iti0202/kt/hotel/Hotel.java
+++ KT/KT2/src/ee/taltech/iti0202/kt/hotel/Hotel.java
@@ -1,4 +1,4 @@
-package ee.taltech.iti0202.kt.hotel.hotel;
+package ee.taltech.iti0202.kt.hotel;
 
 import ee.taltech.iti0202.kt.hotel.rooms.Room;
 
--- KT/KT2/src/ee/taltech/iti0202/kt/hotel/tests/HotelTest.java
+++ KT/KT2/src/ee/taltech/iti0202/kt/hotel/tests/HotelTest.java
@@ -1,6 +1,6 @@
 package ee.taltech.iti0202.kt.hotel.tests;
 
-import ee.taltech.iti0202.kt.hotel.hotel.Hotel;
+import ee.taltech.iti0202.kt.hotel.Hotel;
 import ee.taltech.iti0202.kt.hotel.rooms.Room;
 import ee.taltech.iti0202.kt.hotel.rooms.RoomType;
 import org.junit.jupiter.api.Assertions;
@@ -11,23 +11,24 @@ import java.util.List;
 
 class HotelTest {
     @Test
-    void getAllRooms(){
+    void getAllRooms() {
         Hotel hotel = new Hotel();
         Room room = new Room(1, 5 * 5, RoomType.Type.REGULAR);
-        Room room2= new Room(2, 5 * 4, RoomType.Type.REGULAR);
-        Room room3 = new Room(3, 6 * 5, RoomType.Type.SUITE);
+        Room room2 = new Room(2, 5 * 4, RoomType.Type.REGULAR);
+        Room room3 = new Room(3, 5 * 5, RoomType.Type.SUITE);
         hotel.addRoom(room);
         hotel.addRoom(room2);
         hotel.addRoom(room3);
         List<Room> result = new ArrayList<>(List.of(room, room2, room3));
         Assertions.assertEquals(result, hotel.getAllRooms());
     }
+
     @Test
-    void getAllRooms2(){
+    void getAllRooms2() {
         //Get all rooms when on have same room number
         Hotel hotel = new Hotel();
         Room room = new Room(1, 5 * 5, RoomType.Type.REGULAR);
-        Room room2= new Room(2, 5 * 4, RoomType.Type.REGULAR);
+        Room room2 = new Room(2, 5 * 4, RoomType.Type.REGULAR);
         Room room3 = new Room(2, 6 * 5, RoomType.Type.SUITE);
         hotel.addRoom(room);
         hotel.addRoom(room2);
@@ -35,12 +36,13 @@ class HotelTest {
         List<Room> result = new ArrayList<>(List.of(room, room2));
         Assertions.assertEquals(result, hotel.getAllRooms());
     }
+
     @Test
-    void getTakenRooms(){
+    void getTakenRooms() {
         //Get all rooms when on have same room number
         Hotel hotel = new Hotel();
         Room room = new Room(1, 5 * 5, RoomType.Type.REGULAR);
-        Room room2= new Room(2, 5 * 4, RoomType.Type.REGULAR);
+        Room room2 = new Room(2, 5 * 4, RoomType.Type.REGULAR);
         Room room3 = new Room(3, 6 * 5, RoomType.Type.SUITE);
         hotel.addRoom(room);
         hotel.addRoom(room2);
@@ -50,12 +52,13 @@ class HotelTest {
         List<Room> result = new ArrayList<>(List.of(room, room2));
         Assertions.assertEquals(result, hotel.getTakenRooms());
     }
+
     @Test
-    void getFreeRooms(){
+    void getFreeRooms() {
         //Get all rooms when on have same room number
         Hotel hotel = new Hotel();
         Room room = new Room(1, 5 * 5, RoomType.Type.REGULAR);
-        Room room2= new Room(2, 5 * 4, RoomType.Type.REGULAR);
+        Room room2 = new Room(2, 5 * 4, RoomType.Type.REGULAR);
         Room room3 = new Room(3, 6 * 5, RoomType.Type.SUITE);
         hotel.addRoom(room);
         hotel.addRoom(room2);
@@ -69,12 +72,13 @@ class HotelTest {
         List<Room> result = new ArrayList<>(List.of(room, room2));
         Assertions.assertEquals(result, hotel.getFreeRooms());
     }
+
     @Test
-    void GotSearchedRoom(){
+    void GotSearchedRoom() {
         //Get all rooms when on have same room number
         Hotel hotel = new Hotel();
         Room room = new Room(1, 5 * 5, RoomType.Type.REGULAR);
-        Room room2= new Room(2, 5 * 4, RoomType.Type.REGULAR);
+        Room room2 = new Room(2, 5 * 4, RoomType.Type.REGULAR);
         Room room3 = new Room(3, 6 * 5, RoomType.Type.SUITE);
         hotel.addRoom(room);
         hotel.addRoom(room2);
@@ -85,15 +89,15 @@ class HotelTest {
         room.cancelRoom();
         room2.cancelRoom();
         room3.cancelRoom();
-        Assertions.assertTrue(hotel.searchFreeRooms(4*5));
+        Assertions.assertTrue(hotel.searchFreeRooms(4 * 5));
     }
 
     @Test
-    void GotSearchedRoomButNotFree(){
+    void gotSearchedRoomButNotFree() {
         //Get all rooms when on have same room number
         Hotel hotel = new Hotel();
         Room room = new Room(1, 5 * 5, RoomType.Type.REGULAR);
-        Room room2= new Room(2, 5 * 4, RoomType.Type.REGULAR);
+        Room room2 = new Room(2, 5 * 4, RoomType.Type.REGULAR);
         Room room3 = new Room(3, 6 * 5, RoomType.Type.SUITE);
         hotel.addRoom(room);
         hotel.addRoom(room2);
@@ -101,6 +105,6 @@ class HotelTest {
         room.takeRoom();
         room2.takeRoom();
         room3.takeRoom();
-        Assertions.assertFalse(hotel.searchFreeRooms(4*5));
+        Assertions.assertFalse(hotel.searchFreeRooms(4 * 5));
     }
 }
