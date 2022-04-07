60b5bb4a574e6a47903bc01e29bf4af98468fe61
Sander Pleesi <saplee@ttu.ee>
2022-04-07 18:54:12 +0300

    java

---
+++ KT/KT2/src/ee/taltech/iti0202/kt/hotel/RoomType.java
@@ -0,0 +1,8 @@
+package ee.taltech.iti0202.kt.hotel;
+
+public class RoomType {
+    public enum Type {
+        SUITE, REGULAR
+    }
+}
+
---
+++ KT/KT2/src/ee/taltech/iti0202/kt/hotel/tests/HotelTest.java
@@ -0,0 +1,6 @@
+package ee.taltech.iti0202.kt.hotel.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelTest {
+}
