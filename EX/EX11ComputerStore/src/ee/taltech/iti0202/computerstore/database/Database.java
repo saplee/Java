46ddfbcc5a85dc5014d8daee57c9da3dff44dfd7
Sander Pleesi <saplee@ttu.ee>
2022-04-10 19:08:24 +0300

    java

--- EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
+++ EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
@@ -8,11 +8,10 @@ import ee.taltech.iti0202.computerstore.exceptions.ProductNotFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 
 public class Database {
-    private HashMap<Component, Integer> components = new HashMap<>();
+    private HashMap<Integer, Component> components = new HashMap<>();
     private static Database instance = null;
 
     private Database() {
@@ -26,63 +25,45 @@ public class Database {
     }
 
     public void saveComponent(Component component) throws ProductAlreadyExistsException {
-        if (components.containsKey(component)) {
+        if (components.containsKey(component.getId())) {
             throw new ProductAlreadyExistsException();
         } else {
-            components.put(component, 1);
+            components.put(component.getId(), component);
         }
     }
 
     public void deleteComponent(int id) throws ProductNotFoundException {
-        int startSize = components.size();
-        for (Component component : components.keySet()) {
-            if (component.getId() == id) {
-                components.remove(component);
-            }
-
-        }
-        if (components.size() == startSize) {
+        if (!components.containsKey(id)) {
             throw new ProductNotFoundException();
+        } else if (components.containsKey(id)) {
+            components.remove(id);
         }
     }
 
     public void increaseComponentStock(int id, int amount) throws ProductNotFoundException {
-        boolean componentIncreased = false;
         if (amount <= 0) {
             throw new IllegalArgumentException();
-        }
-        for (Component component : components.keySet()) {
-            if (component.getId() == id) {
-                components.put(component, components.get(component) + amount);
-                component.setAmount(components.get(component));
-                componentIncreased = true;
-            }
-        }
-        if (!componentIncreased) {
+        } else if (!components.containsKey(id)) {
             throw new ProductNotFoundException();
+        } else if (components.containsKey(id)) {
+            components.get(id).setAmount(components.get(id).getAmount() + amount);
         }
     }
 
     public void decreaseComponentStock(int id, int amount) throws OutOfStockException, ProductNotFoundException {
-        boolean componentDecreased = false;
         if (amount <= 0) {
             throw new IllegalArgumentException();
-        }
-        for (Component component : components.keySet()) {
-            if (component.getId() == id && components.get(component) >= amount) {
-                components.put(component, components.get(component) + amount);
-                componentDecreased = true;
-            } else if (component.getId() == id && components.get(component) < amount) {
-                throw new OutOfStockException();
-            }
-        }
-        if (!componentDecreased) {
+        } else if (!components.containsKey(id)) {
             throw new ProductNotFoundException();
+        } else if (components.containsKey(id) && components.get(id).getAmount() < amount) {
+            throw new OutOfStockException();
+        } else if (components.containsKey(id) && components.get(id).getAmount() >= amount) {
+            components.get(id).setAmount(components.get(id).getAmount() - amount);
         }
     }
 
     public Map<Integer, Component> getComponents() {
-        return components.entrySet().stream().collect(Collectors.toMap(HashMap.Entry::getValue, HashMap.Entry::getKey));
+        return components;
     }
 
     public void resetEntireDatabase() {
