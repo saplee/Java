57c4bf5093738d4d2a8d6751eb2ddc5e9e77ec4e
Sander Pleesi <saplee@ttu.ee>
2022-04-10 18:19:47 +0300

    java

--- EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
+++ EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
@@ -85,6 +85,7 @@ public class Database {
 
     public void resetEntireDatabase() {
         components.clear();
+        Component.setAndIncrementNextId();
     }
 
     public void saveToFile(String location) {
