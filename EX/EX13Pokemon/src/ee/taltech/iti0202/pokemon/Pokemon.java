6959995964baa9253e37a78f2b50f8b2727992fc
Sander Pleesi <saplee@ttu.ee>
2022-05-04 18:13:03 +0300

    java

---
+++ EX/EX13Pokemon/src/ee/taltech/iti0202/pokemon/Pokemon.java
@@ -0,0 +1,4 @@
+package ee.taltech.iti0202.pokemon;
+
+public class Pokemon {
+}
--- EXAM/EXAM0/src/ee/taltech/iti0202/exam/Exam.java
+++ EXAM/EXAM0/src/ee/taltech/iti0202/exam/Exam.java
@@ -1,8 +1,6 @@
 package ee.taltech.iti0202.exam;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Exam {
 
@@ -35,5 +33,38 @@ public class Exam {
             }
         }
         return result;
+    }
+
+    /**
+     * 02
+     *
+     * Write a method that analyzes input String and returns all pairs of same letter that is present as lower-case and upper-case in input String.
+     * Returned letter pairs have to be in alphabetic order. If there are multiple same letter pairs, then return only one. If there are no suitable pairs, return "".
+     * Take latin alphabet 'a' - 'z' as base.
+     * mixedPairs("abcABD") => "AaBb" (a and b are represented by both lowe and upper cased letter)
+     * mixedPairs("aaaAAAaaaa") => "Aa"
+     * mixedPairs("tere") => ""
+     * mixedPairs("bBaacA") => "AaBb" (result is in alphabetic order, although in input String, b is earlier than a).
+     * @param input
+     * @return
+     */
+    public static String mixedPairs(String input) {
+        List<String> result = new ArrayList<>();
+        String lastWord = "";
+        for (int i = 0; i < input.length(); i++) {
+            String upper = String.valueOf(Character.toUpperCase(input.charAt(i)));
+            String lower = String.valueOf(Character.toLowerCase(input.charAt(i)));
+            if (!result.contains(upper) && !result.contains(lower) && input.contains(upper) && input.contains(lower)){
+                result.add(lower);
+            }
+        }
+        if (result.size()==0){
+            return lastWord;
+        }else {
+            List<String> newResult = result.stream().sorted().toList();
+            for (String s : newResult){
+                lastWord += s.toUpperCase()+s;
+            }
+        }return lastWord;
     }
 }
