8744afdb5d2101749c81d0d8822fc375f65e7ab6
Sander Pleesi <saplee@ttu.ee>
2022-05-12 02:01:08 +0300

    java

--- LX/LX02PersonStatistics/src/ee/taltech/iti0202/personstatistics/CsvToPersonMappingException.java
+++ LX/LX02PersonStatistics/src/ee/taltech/iti0202/personstatistics/CsvToPersonMappingException.java
@@ -1,5 +1,5 @@
 package ee.taltech.iti0202.personstatistics;
 
-public class CsvToPersonMappingException extends RuntimeException{
+public class CsvToPersonMappingException extends RuntimeException {
 
 }
