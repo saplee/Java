55d840b37969d178c98d1e939649aa7024a4d93b
Sander Pleesi <saplee@ttu.ee>
2022-05-14 19:30:41 +0300

    java

--- EXAM/OOP1/src/ee/taltech/iti0202/store/exceptions/CannotAddProductToShop.java
+++ EXAM/OOP1/src/ee/taltech/iti0202/store/exceptions/CannotAddProductToShop.java
@@ -2,7 +2,7 @@ package ee.taltech.iti0202.store.exceptions;
 
 public class CannotAddProductToShop extends Throwable {
 
-    public CannotAddProductToShop(){
+    public CannotAddProductToShop() {
     }
 
     public String getMessage() {
