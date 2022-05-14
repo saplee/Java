ea7ec95683f51487459e0ccfcb3ac61995c02b4b
Sander Pleesi <saplee@ttu.ee>
2022-05-14 19:19:58 +0300

    java

--- EXAM/OOP1/src/ee/taltech/iti0202/store/Main.java
+++ EXAM/OOP1/src/ee/taltech/iti0202/store/Main.java
@@ -9,16 +9,19 @@ import ee.taltech.iti0202.store.shops.FoodShop;
 
 public class Main {
     private static final double PROFIT = 1000;
-    private static final double PROFIT_OF_SECOND_SHOP = 10000;
-    private static final double PRICE_OF_TV = 570;
+    private static final double SECOND_SHOP_PROFIT = 10000;
+    private static final double TV_PRICE = 570;
+    private static final double PAINKILLER_PRICE = 8;
+    private static final int AGO_AGE = 29;
+
     public static void main(String[] args) throws CannotAddProductToShop {
         FoodShop foodShop = new FoodShop("Selver", PROFIT);
-        AllShop allShop = new AllShop("Prisma", PROFIT_OF_SECOND_SHOP);
-        Product product = new Product("TV", PRICE_OF_TV, ProductType.ELECTRONICS);
-        Product product1 = new Product("Painkiller", 8, ProductType.MEDICINE);
+        AllShop allShop = new AllShop("Prisma", SECOND_SHOP_PROFIT);
+        Product product = new Product("TV", TV_PRICE, ProductType.ELECTRONICS);
+        Product product1 = new Product("Painkiller", PAINKILLER_PRICE, ProductType.MEDICINE);
         Product product2 = new Product("Spoon", 3, ProductType.HOUSE_HOLD);
         Product product3 = new Product("Apple", 1, ProductType.FOOD);
-        Client client = new Client("Ago", 29, 1000);
+        Client client = new Client("Ago", AGO_AGE, 1000);
         Client client1 = new Client("Annemari", 20, 1000);
 
 
