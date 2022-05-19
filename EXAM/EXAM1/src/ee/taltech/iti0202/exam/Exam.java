53ee7d8e9bad93e35626e6f925625289d17ac4ca
Sander Pleesi <saplee@ttu.ee>
2022-05-20 01:38:28 +0300

    java

---
+++ EXAM/EXAM1/src/ee/taltech/iti0202/exam/Exam.java
@@ -0,0 +1,59 @@
+package ee.taltech.iti0202.exam;
+
+import java.util.List;
+
+public class Exam {
+
+    /**
+     * Given a list of numbers, count how many 2-s are alone (no 2 before or after it).
+     * <p>
+     * countSingleTwos([2, 2, 1, 3]) => 0
+     * countSingleTwos([7, 6, 1, 3]) => 0
+     * countSingleTwos([2, 2, 1, 2]) => 1
+     * countSingleTwos([2, 2, 2, 1, 3, 2, 1, 2]) => 2
+     */
+    public static int countSingleTwos(List<Integer> numbers) {
+        return -2;
+    }
+
+    /**
+     * Write a method that takes a string and decodes it.
+     * The string may contain some numbers.
+     * All numbers need to be replaced with a corresponding letter from the alphabet.
+     * Each number n references to n-th character of the lowercase alphabet (abcdefghijklmnopqrstuvwxyz).
+     * If n is out of bounds, then it should start from "a" again. (0, 26 and 52 correspond to "a")
+     * <p>
+     * Examples:
+     * decodeMessage("0") => "a"
+     * decodeMessage("0b2d4f6") => "abcdefg"
+     * decodeMessage("h8") => "hi"
+     * decodeMessage("11o11") => "lol"
+     * decodeMessage("h8 th4r30 p17ogramme43") => "hi there programmer"
+     * decodeMessage(":14 19h8s 8s 84e45t34n58 54oo37e523423") => ":o this is getting cooler"
+     * decodeMessage("This one doesn't need to be changed!") => "This one doesn't need to be changed!"
+     *
+     * @param message the message that needs to be decoded
+     * @return decoded message
+     */
+    public static String decodeMessage(String message) {
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        String number = "";
+        String result = "";
+        for (int i = 0; i < message.length(); i++) {
+            if (!Character.isDigit(message.charAt(i)) && number.length() == 0) {
+                result += message.charAt(i);
+            } else if (Character.isDigit(message.charAt(i))) {
+                number += message.charAt(i);
+            } else if (!Character.isDigit(message.charAt(i)) && number.length() != 0) {
+                result += alpha.charAt(Integer.parseInt(number) % 26);
+                result += message.charAt(i);
+                result = "";
+            }
+        }
+        if (result.length() != 0) {
+            result += alpha.charAt(Integer.parseInt(number) % 26);
+        }
+        return result;
+    }
+}
+
